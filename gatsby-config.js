e2bc605bd83098c2c4f1efe35d2d0bc05acb8843
Dave Laird <kiwiupover@gmail.com>
2019-09-12 08:41:43 -0700

    remove netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,6 @@ module.exports = {
     `gatsby-plugin-offline`,
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    `gatsby-plugin-netlify`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
